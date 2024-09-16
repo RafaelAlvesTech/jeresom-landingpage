bbd59ff5fb5b112c731fc8984110637b030ffb8b
RafaelAlvesTech <rafaelpereiraalves@ufba.br>
2024-09-16 20:32:13 -0300

    style: :art: index repair

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,33 +23,34 @@ import { BlogPageComponent } from './components/pages/blog-page/blog-page.compon
 import { BlogDetailsPageComponent } from './components/pages/blog-details-page/blog-details-page.component';
 
 const routes: Routes = [
-    {path: '', component: HomeDemoOneComponent},
-    {path: 'index-2', component: HomeDemoTwoComponent},
-    {path: 'about', component: AboutPageComponent},
-    {path: 'services', component: ServicesPageComponent},
-    {path: 'service-details', component: ServiceDetailsPageComponent},
-    {path: 'testimonials', component: TestimonialsPageComponent},
-    {path: 'send-money', component: SendMoneyPageComponent},
-    {path: 'team', component: TeamPageComponent},
-    {path: 'faq', component: FaqPageComponent},
-    {path: 'saving-goals', component: SavingGoalsPageComponent},
-    {path: 'cash-back', component: CashBackPageComponent},
-    {path: 'pricing', component: PricingPageComponent},
-    {path: 'help', component: HelpPageComponent},
-    {path: 'login', component: LoginPageComponent},
-    {path: 'register', component: RegisterPageComponent},
-    {path: 'privacy-policy', component: PrivacyPolicyPageComponent},
-    {path: 'terms-conditions', component: TermsConditionsPageComponent},
-    {path: 'blog', component: BlogPageComponent},
-    {path: 'blog-details', component: BlogDetailsPageComponent},
-    {path: 'contact', component: ContactPageComponent},
+    { path: 'index-2', redirectTo: '', pathMatch: 'full' },
+    { path: '', component: HomeDemoTwoComponent },
+    { path: '', component: HomeDemoOneComponent },
+    { path: 'about', component: AboutPageComponent },
+    { path: 'services', component: ServicesPageComponent },
+    { path: 'service-details', component: ServiceDetailsPageComponent },
+    { path: 'testimonials', component: TestimonialsPageComponent },
+    { path: 'send-money', component: SendMoneyPageComponent },
+    { path: 'team', component: TeamPageComponent },
+    { path: 'faq', component: FaqPageComponent },
+    { path: 'saving-goals', component: SavingGoalsPageComponent },
+    { path: 'cash-back', component: CashBackPageComponent },
+    { path: 'pricing', component: PricingPageComponent },
+    { path: 'help', component: HelpPageComponent },
+    { path: 'login', component: LoginPageComponent },
+    { path: 'register', component: RegisterPageComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyPageComponent },
+    { path: 'terms-conditions', component: TermsConditionsPageComponent },
+    { path: 'blog', component: BlogPageComponent },
+    { path: 'blog-details', component: BlogDetailsPageComponent },
+    { path: 'contact', component: ContactPageComponent },
     // Here add new pages component
 
-    {path: '**', component: NotFoundComponent} // This line will remain down from the whole pages component list
+    { path: '**', component: NotFoundComponent }, // This line will remain down from the whole pages component list
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
